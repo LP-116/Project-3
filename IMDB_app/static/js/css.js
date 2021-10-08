64e1b1d9d47fdab4e7484a44b1596747dace08b0
Leanne <leanne_porter@hotmail.com>
2021-10-08 14:01:51 +1100

    updating code

---
+++ IMDB_app/static/js/css.js
@@ -0,0 +1,12 @@
+// var i = 0;
+// var txt = "\"We dont make movies to make money, we make money to make more movies.\" -- Walt Disney"
+// var speed = 80; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("text-effect2").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
