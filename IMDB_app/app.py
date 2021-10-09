058c1b40ac79cd6cda39dfaab62ad5791ee5270c
Leanne <leanne_porter@hotmail.com>
2021-10-09 11:07:50 +1100

    starting web scraping

--- IMDB_app/app.py
+++ IMDB_app/app.py
@@ -7,19 +7,33 @@ import os
 
 from flask import Flask, jsonify, render_template, redirect
 from flask_pymongo import PyMongo
+import imdb_scrape
 
 
 # Create Flask
 app = Flask(__name__)
 
+mongo = PyMongo(app, uri="mongodb://localhost:27017/imdb_app")
 
 
-# Homepage Route. Grabs one entry from Mongo database for the news headlines.
 @app.route("/")
 def welcome():
 
+    imdb_data = mongo.db.data.find_one()
+
     
-    return render_template("index.html")
+    return render_template("index.html", imdb=imdb_data)
+
+
+
+@app.route("/scrape")
+def scrape():
+
+    get_data = imdb_scrape.scrape_all()
+
+    mongo.db.data.update({}, get_data, upsert=True)
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
---
+++ IMDB_app/imdb_scrape.py
@@ -0,0 +1,12 @@
+import os
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+import datetime as dt
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+
+def imdb_hits(browser):
