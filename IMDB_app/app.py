77aa56448c622401a642a6387e64d41d0121fa8a
Leanne <leanne_porter@hotmail.com>
2021-10-10 13:25:14 +1100

    transforming csv files for graphs

--- IMDB_app/app.py
+++ IMDB_app/app.py
@@ -39,14 +39,28 @@ def scrape():
 @app.route("/rating")
 def rating():
 
-    rating_data = "graph1-highest_rated.csv"
-    rating_df = pd.read_csv(rating_data)
+    rating_csv = "graph1-highest_rated.csv"
+    rating_df = pd.read_csv(rating_csv)
     rating_df = rating_df.sort_values("avg_vote", ascending=False)
     
     rating_data = rating_df.to_dict('records')
 
 
     return jsonify(rating_data)
+
+
+@app.route("/profit")
+def profit():
+
+    profit_csv = "graph2-profit.csv"
+    profit_df = pd.read_csv(profit_csv)
+    
+    profit_data = profit_df.to_dict('records')
+
+
+    return jsonify(profit_data)
+
+
 
 
 
