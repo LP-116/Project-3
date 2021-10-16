4e413c3e8e43dec8d76f872bc445296afb5ca451
Leanne <leanne_porter@hotmail.com>
2021-10-16 16:37:50 +1100

    updating code

--- IMDB_app/static/js/app.js
+++ IMDB_app/static/js/app.js
@@ -1,8 +1,8 @@
+
+// Function used to generate the Country and year dropdown lists.
 function init() {
 
     d3.json("/country").then((item) => {
-
-        // console.log(item)
 
         country_list = []
 
@@ -42,6 +42,7 @@ function init() {
             .property("value", item);
         });
         
+        // Once the dropdown lists are generated, run the functions to build the graphs and update the stats.
         buildGraph();
         updatestats();
         buildGraph2()
@@ -51,10 +52,10 @@ function init() {
 };
 
 
-// This function builds the bar and line graphs.
+// This function builds the bar graphs in the introductory analysis section.
 function buildGraph() {
     
-    // Reading the incidents route data.
+    // Reading the votes route data.
     d3.json("/votes").then((data) => {
 
         // Clearing the existing chart space to avoid overlap issues.
@@ -72,7 +73,7 @@ function buildGraph() {
         votes_list = []
         year_list =[]
 
-        // Everytime the suburb in the dropdown box is matched to the json data, push the required part into the matching list.
+        // Everytime the country and year in the dropdown box is matched to the json data, push the required part into the matching list.
         for (var i in data) {
 
 
@@ -133,7 +134,7 @@ function buildGraph() {
                     beginAtZero: false,
                     grouped: true,
                     userCallback: function (value, index, values) {
-                        // Convert the number to a string and splite the string every 3 charaters from the end
+                        // Convert the number to a string and split the string every 3 charaters from the end
                         value = value.toString();
                         value = value.split(/(?=(?:...)*$)/);
 
@@ -150,13 +151,13 @@ function buildGraph() {
     }
 })})
 
-    // Reading the incidents route data.
+    // Reading the profit route data.
     d3.json("/profit").then((data) => {
 
         // Clearing the existing chart space to avoid overlap issues.
         document.querySelector("#chart2").innerHTML = '<canvas id="myChart1"></canvas>';
 
-        // Getting the suburb value in the dropdown box.
+        // Getting the country and year value in the dropdown box.
         var idSelect = d3.select("#selDataset").property("value");
         var idSelect2 = d3.select("#selDataset2").property("value");
 
@@ -167,7 +168,7 @@ function buildGraph() {
         movie_list = []
         profit_list = []
 
-        // Everytime the suburb in the dropdown box is matched to the json data, push the required part into the matching list.
+        // Everytime the country and year in the dropdown box is matched to the json data, push the required part into the matching list.
         for (var i in data) {
 
 
@@ -193,7 +194,7 @@ function buildGraph() {
 
         
         // Create the graph using Chart.js
-               var myChart1 = new Chart("myChart1", {
+       var myChart1 = new Chart("myChart1", {
         type: "horizontalBar",
 
         data: {
@@ -228,7 +229,7 @@ function buildGraph() {
                     beginAtZero: false,
                     grouped: true,
                     userCallback: function (value, index, values) {
-                        // Convert the number to a string and splite the string every 3 charaters from the end
+                        // Convert the number to a string and split the string every 3 charaters from the end
                         value = value.toString();
                         value = value.split(/(?=(?:...)*$)/);
 
@@ -248,13 +249,13 @@ function buildGraph() {
 
 }
 
-// This function is used to update the stats_data.
+// This function is used to update the stats data (top 10 movies by rating).
 function updatestats() {
     
 
     d3.json("/rating").then((data) => {
 
-        // Getting the suburb value in the dropdown box.
+        // Getting the country and year value in the dropdown box.
         var idSelect = d3.select("#selDataset").property("value");
         var idSelect2 = d3.select("#selDataset2").property("value");
 
@@ -262,7 +263,7 @@ function updatestats() {
         movie_list = []
         rating_list = []
 
-        // Everytime the suburb in the dropdown box is matched to the json data, push the required part into the matching list.
+        // Everytime the country and year in the dropdown box is matched to the json data, push the required part into the matching list.
         for (var i in data) {
 
 
@@ -279,19 +280,21 @@ function updatestats() {
 
         };
 
+        // Splitting the results to show the top 5 for first half of the card.
         var top5_movies = movie_list.slice(0,5);
         var top5_ratings = rating_list.slice(0,5);
 
+        // Creating a dictionary to store the rating and movie together
         var combined = top5_movies.map(function(e, i) {
             return [e, top5_ratings[i]];
           });
 
         console.log(combined)
         
-        d3.select("#cardyear").text(idSelect2)
-
+        // Selecting the card.
         var infoBox = d3.select("#card1");
 
+        // Clearing the existing data.
         infoBox.html("");
 
         number = 1
@@ -307,6 +310,7 @@ function updatestats() {
             });
 
 
+        // Repeating the above steps for the second half of the card.
         var top10_movies = movie_list.slice(6,11);
         var top10_ratings = rating_list.slice(6,11);
     
@@ -335,13 +339,13 @@ function updatestats() {
 
 function buildGraph2() {
     
-    // Reading the incidents route data.
+    // Reading the genre route data.
     d3.json("/genre").then((data) => {
 
         // Clearing the existing chart space to avoid overlap issues.
         document.querySelector("#chart3").innerHTML = '<canvas id="myChart3"></canvas>';
 
-        // Getting the suburb value in the dropdown box.
+        // Getting the year value in the dropdown box.
         var idSelect3 = d3.select("#selDataset3").property("value");
 
         console.log(idSelect3);
@@ -351,7 +355,7 @@ function buildGraph2() {
         genre_list = []
         income_list = []
 
-        // Everytime the suburb in the dropdown box is matched to the json data, push the required part into the matching list.
+        // Everytime the year in the dropdown box is matched to the json data, push the required part into the matching list.
         for (var i in data) {
 
 
@@ -368,6 +372,7 @@ function buildGraph2() {
 
         }
 
+        // Create the bar chart using chart.js
         var colors = ['rgb(230, 0, 0)', 'rgb(255, 128, 0)', 'rgb(0, 64, 255)', 'rgb(128, 0, 255)', 'rgb(0, 153, 0)', 'rgb(0, 204, 255)', 'rgb(255, 102, 204)']
         var myChart1 = new Chart("myChart3", {
         type: "bar",
@@ -404,7 +409,7 @@ function buildGraph2() {
                     beginAtZero: true,
                     grouped: true,
                     userCallback: function (value, index, values) {
-                        // Convert the number to a string and splite the string every 3 charaters from the end
+                        // Convert the number to a string and split the string every 3 charaters from the end
                         value = value.toString();
                         value = value.split(/(?=(?:...)*$)/);
 
@@ -419,13 +424,13 @@ function buildGraph2() {
         }
     })
 })
-
+        // Reading the pie genre route data. 
         d3.json("/piegenre").then((data) => {
 
             // Clearing the existing chart space to avoid overlap issues.
             document.querySelector("#chart4").innerHTML = '<canvas id="myChart4"></canvas>';
 
-            // Getting the suburb value in the dropdown box.
+            // Getting the year value in the dropdown box.
             var idSelect3 = d3.select("#selDataset3").property("value");
 
             console.log(idSelect3);
@@ -435,7 +440,7 @@ function buildGraph2() {
             genre_list = []
             count_list = []
 
-            // Everytime the suburb in the dropdown box is matched to the json data, push the required part into the matching list.
+            // Everytime the year in the dropdown box is matched to the json data, push the required part into the matching list.
             for (var i in data) {
 
 
@@ -447,11 +452,12 @@ function buildGraph2() {
 
             }
 
+            // Getting the top5 results.
             var top5_genre = genre_list.slice(0,5);
             var top5_count = count_list.slice(0,5);
 
 
-
+            // creating the pie chart using chart.js.
             var myChart4 = new Chart("myChart4", {
                         type: 'pie',
                         data:  {
@@ -495,6 +501,8 @@ function buildGraph2() {
         })
     }
 
+
+
 // Each time the drop down selection is changed, run the functions.
 function optionChanged()
 { 
@@ -522,5 +530,6 @@ function stopSpinner1() {
 
 
 // Run the init function on webpage load.
+
 init();
 
