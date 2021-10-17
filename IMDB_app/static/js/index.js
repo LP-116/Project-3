71d720b9132bd951d02c8cb5b83becbab3d884c6
Leanne <leanne_porter@hotmail.com>
2021-10-17 13:02:40 +1100

    setting up external mongodb

---
+++ IMDB_app/static/js/index.js
@@ -0,0 +1,15 @@
+function startSpinner1() {
+    // Code to make the spinner start
+    $("#filter-btn1").prop("disabled", true);
+    $("#filter-btn1").html(
+        `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>&nbsp Loading...`
+    );
+}
+
+function stopSpinner1() {
+    // Code to make the spinner stop
+    // (i.e., return the button to its original state)
+    $("#filter-btn1").prop("disabled", false);
+    $("#filter-btn1").html('&nbsp &nbsp &nbsp View Results &nbsp &nbsp &nbsp');
+}
+
